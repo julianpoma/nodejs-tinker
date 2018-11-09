e2e8c0f022a6aa636e3c9e0bb81312e8b2d6183d
Julian <poma.nob@gmail.com>
2018-11-09 14:19:06 -0300

    Basic routes

--- app.js
+++ app.js
@@ -3,7 +3,16 @@ const express = require('express');
 const app = express();
 
 app.use((req, res, next) => {
-    console.log("Hi!");
+    console.log("heh!");
+    next()
+})
+
+app.use('/users', (req, res, next) => {
+    res.send('<h1>Users list<h1>');
+})
+
+app.use('/', (req, res, next) => {
+    res.send('<h1>Dashboard<h1>');
 })
 
 app.listen(3000);
