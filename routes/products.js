35599ec457c653b1923eb08de771cdf052e0d3bf
Julian <poma.nob@gmail.com>
2018-11-14 19:14:33 -0300

    New Styles

--- routes/products.js
+++ routes/products.js
@@ -6,14 +6,12 @@ const router = express.Router();
 const rootDir = require('../util/path');
 
 router.get('/products', (req, res, next) => {
-    res.sendFile(path.join(rootDir, 'views', 'products', 'create.html'));
+	res.sendFile(path.join(rootDir, 'views', 'products', 'create.html'));
 });
 
 router.post('/products', (req, res, next) => {
-    console.log(req.body);
-    res.redirect('/');
+	console.log(req.body);
+	res.redirect('/');
 });
 
 module.exports = router;
-
-//https://stackoverflow.com/questions/50886914/visual-studio-code-installation-error-on-ubuntu-18-04
